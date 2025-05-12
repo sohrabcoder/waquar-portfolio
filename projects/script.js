a8db8966247e7ec734d210d298680d5b208e5334
Md Rafiuddin <rafimech40@gmail.com>
2025-05-12 22:32:30 +0530

    waquar-portfolio

--- projects/script.js
+++ projects/script.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 
 document.addEventListener("visibilitychange", function () {
   if (document.visibilityState === "visible") {
-    document.title = "Projects | Portfolio Md Rafiuddin";
+    document.title = "Projects | Portfolio Waquar Ahmad";
     $("#favicon").attr("href", "assets/images/favicon.png");
   } else {
     document.title = "Come Back To Portfolio";
